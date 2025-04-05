005c1ea9826b8f90652e6892e9af440f3013416b
axel-mazariegos <amazariegoslicardie@byupathway.edu>
2025-04-05 07:56:32 -0600

    Handling DOM Events

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,23 +2,46 @@
 // that hold references to the input, button, and list elements.
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('list');
+const list = document.querySelector('#list');
 
-// 6. Create a li element that will hold each entry's chapter title 
-// and an associated delete button.
-const li = document.createElement('li');
 
-// 7. Create a delete button.
-const deleteButton = document.createElement('button');
+// Handling DOM Events
 
-// 8. Populate the li element variable's textContent or innerHTML with the input value.
-li.textContent = input.value;
+// 2. Create a click event listener for the Add Chapter button using an addEventListener.
+button.addEventListener('click', function () {
+    
+    if (input.value.trim() !== '') {
+        // 6. Create a li element that will hold each entry's chapter title 
+        // and an associated delete button.
+        const li = document.createElement('li');
 
-// 9. Populate the button textContent with a ❌.
-deleteButton.textContent = '❌';
+        // 7. Create a delete button.
+        const deleteButton = document.createElement('button');
 
-// 10. Append the li element variable with the delete button.
-li.append(deleteButton);
+        // 8. Populate the li element variable's textContent or innerHTML with the input value.
+        li.textContent = input.value;
 
-// 11. Append the li element variable to the unordered list in your HTML.
-list.append(li);
+        // 9. Populate the button textContent with a ❌.
+        deleteButton.textContent = '❌';
+
+        // 10. Append the li element variable with the delete button.
+        li.append(deleteButton);
+
+        // 11. Append the li element variable to the unordered list in your HTML.
+        list.append(li);
+
+        // Add an event listener to the delete button that removes the li element when clicked.
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        // Change the input value to nothing or the empty string to clean up the interface for the user.
+        input.value = '';
+
+        // Whether or not a list item was created, the focus (active cursor) should be sent to the input element.
+        input.focus();
+    }
+
+});
+
