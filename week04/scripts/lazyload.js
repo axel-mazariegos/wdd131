23a3724ecabdf7ce4b9d46b45f41a804b85d258c
axel-mazariegos <amazariegoslicardie@byupathway.edu>
2025-04-11 23:13:18 -0600

    Lazyloading Images

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,14 @@
+// Select the span element by its ID
+const year = document.getElementById("currentyear");
+
+// Create a Date object
+const today = new Date();
+
+// Update the inner HTML with the current year
+year.innerHTML = today.getFullYear();
+
+// Select the paragraph where the last modified date will be displayed
+const lastModified = document.getElementById("lastModified");
+
+// Set the innerHTML to the lastModified date from the document object
+lastModified.innerHTML = `Last Modification: ${document.lastModified}`;
